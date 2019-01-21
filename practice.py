47a4ddc15373546331605754ff9e7932cf36f335
kamunyulucia <46862697+kamunyulucia@users.noreply.github.com>
2019-01-21 16:16:04 +0300

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,21 @@
+#!/bin/python
+
+#concatenate
+x='4'
+y="2"
+print(x +'&'+ y)
+print(x+y)
+
+#repeat
+
+print(x*3)
+
+#boolean true and false
+x=4
+y=2
+
+if x>y:
+    print("false")
+    else:
+        print("true")
+
